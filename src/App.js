ea4972f4a001e3a3422b5877517237fa8cee38ba
Arjun <lallarjun@gmail.com>
2017-08-07 01:05:55 +0000

    geolocate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentWillMount() {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+      let crd = pos.coords;
+      this.setState(lat:crd.latitude);
+      this.setState(lon:crd.longitude);
+      console.log(crd)
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options)
+  }
+
   render() {
     return (
       <div className="App">
