5375988c2282b9a88891a4716928cdbed956beb0
Arjun <lallarjun@gmail.com>
2017-08-12 20:30:54 +0000

    intial redux setup

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import { CollapsibleItem } from 'react-materialize'
+import { CollapsibleItem, Row, Col, Divider } from 'react-materialize'
+require('datejs');
 
 class Pizza extends Component{
 
   render() {
     return (
       <CollapsibleItem header={this.props.pizza.name} icon='local_pizza'>
-        PIZZA GET YOUR PIZZA
+        <Row>
+          <Col s={8}>
+            <span dangerouslySetInnerHTML={{__html:this.props.pizza.description}}></span>
+          </Col>
+          <Col s={4}>
+            {this._findDate(this.props.pizza.time)}
+            <Divider/>
+            {this.props.pizza.venue.address_1}
+          </Col>
+        </Row>
       </CollapsibleItem>
     )
+  }
+
+  _findDate = (date) => {
+    let d = new Date(date);
+    console.log(d)
+    return d.toLocaleString();
   }
 };
 
